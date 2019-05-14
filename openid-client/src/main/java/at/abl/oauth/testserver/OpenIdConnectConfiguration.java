2fa91fbe398d3449e839f68d7cd209af06173a4a
Matthias Ableidinger <Matthias.Ableidinger@catalysts.cc>
2019-05-14 16:05:06 +0200

    scopes as parameters

--- openid-client/src/main/java/at/abl/oauth/testserver/OpenIdConnectConfiguration.java
+++ openid-client/src/main/java/at/abl/oauth/testserver/OpenIdConnectConfiguration.java
@@ -9,7 +9,10 @@ import org.springframework.security.oauth2.client.resource.OAuth2ProtectedResour
 import org.springframework.security.oauth2.client.token.grant.code.AuthorizationCodeResourceDetails;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableOAuth2Client;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Configuration
 @EnableOAuth2Client
@@ -30,6 +33,10 @@ public class OpenIdConnectConfiguration
     @Value("${openid.redirectUri}")
     private String redirectUri;
 
+    @Value("${openid.additionalScopes:}")
+    private String additionalScopes;
+
+
     @Bean
     public OpenIdConnectFilter openIdConnectFilter(OAuth2RestTemplate oAuth2RestTemplate)
     {
@@ -46,10 +53,18 @@ public class OpenIdConnectConfiguration
         details.setClientSecret(clientSecret);
         details.setAccessTokenUri(accessTokenUri);
         details.setUserAuthorizationUri(userAuthorizationUri);
-        details.setScope(Arrays.asList("openid", "email"));
+        details.setScope(getScopes());
         details.setPreEstablishedRedirectUri(redirectUri);
         details.setUseCurrentUri(false);
         return details;
+    }
+
+    private List<String> getScopes()
+    {
+        List<String> scopes = new ArrayList<>();
+        scopes.add("openid");
+        scopes.addAll(Arrays.asList(additionalScopes.split(",")).stream().filter(s -> !s.isEmpty()).collect(Collectors.toList()));
+        return scopes;
     }
 
     @Bean
